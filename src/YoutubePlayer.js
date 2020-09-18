a00ef46d3f21f8d7ae7a29f0c99e3df5af451832
TatsumiKotaro <1186787@st.anan-nct.ac.jp>
2020-09-18 22:34:05 +0900

    0918-1

--- src/YoutubePlayer.js
+++ src/YoutubePlayer.js
@@ -111,7 +111,9 @@ class YoutubePlayer extends React.Component {
             })}
           </div>
           <div className="pinbutton">
-            <a href="./Pin.svg"></a>
+            <svg width="150" height="150" viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="75" cy="75" r="75" fill="#FE5600"/>
+            </svg>
           </div>
         </div>
         <div className="chat">
