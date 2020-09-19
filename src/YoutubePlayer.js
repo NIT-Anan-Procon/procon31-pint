f60092e113c77ec115325184b7213800e0637e52
TatsumiKotaro <1186787@st.anan-nct.ac.jp>
2020-09-19 23:20:55 +0900

    0919

--- src/YoutubePlayer.js
+++ src/YoutubePlayer.js
@@ -3,10 +3,7 @@ import YouTube from 'react-youtube';
 import axios from 'axios';
 
 import Pin from "./Pin"
-import Controller from "./Controller";
-import PinController from "./PinController";
 import ChatContainer from "./ChatContainer";
-import Header from "./Header";
 
 import './YoutubePlayer.css';
 
@@ -86,17 +83,31 @@ class YoutubePlayer extends React.Component {
       <>
       <div className="header">
         <div className="logo">
-          <img scr="./image/pint ロゴ最終版.png" alt="Pintロゴ" />
+          <img src="./pint ロゴ最終版.png" alt="Pintロゴ" />
         </div>
       </div>
       <div className="all">
         <div className="youtube">
           <YouTube videoId={this.props.videoId} opts={opts} onReady={(event) => this._onReady(event)} />
           <div className="seekbar">
+            <div className="redbar">
 
+            </div>
           </div>
-          <div className="pinhighlight">
+          <div>
+            <div className="pinhighlight">
 
+            </div>
+            <div className="pinbutton">
+              <svg width="150" height="150" viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="75" cy="75" r="75" fill="#FE5600"/>
+              </svg>
+              <div className="pinbuttonChild">
+                <svg width="68" height="100" viewBox="0 0 68 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M33.6633 0C15.0691 0 0 15.0693 0 33.6633C0 52.6402 9.40586 56.6832 19.802 69.3068C32.2367 84.4064 33.6633 100 33.6633 100C33.6633 100 35.0898 84.4064 47.5246 69.307C57.9207 56.6832 67.3266 52.6402 67.3266 33.6635C67.3266 15.0693 52.2574 0 33.6633 0ZM33.6633 45.2971C27.2381 45.2971 22.0297 40.0885 22.0297 33.6635C22.0297 27.2385 27.2383 22.0299 33.6633 22.0299C40.0883 22.0299 45.2969 27.2385 45.2969 33.6635C45.2969 40.0885 40.0883 45.2971 33.6633 45.2971Z" fill="blue"/>
+                </svg>
+              </div>
+            </div>
           </div>
           <div className="pin">
             {this.state.pins.map((pin, index) => {
@@ -112,18 +123,22 @@ class YoutubePlayer extends React.Component {
               )
             })}
           </div>
-          <div className="pinbutton">
-            <svg width="150" height="150" viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="75" cy="75" r="75" fill="#FE5600"/>
-            </svg>
-          </div>
         </div>
-        <div className="chat">
-          <ChatContainer
-            pinID={this.state.pinID}
-            messages={this.state.messages}
-            syncMessage={this.syncMessage}
-          />
+        <div>
+          <div className="pintitlebox">
+            <h2>ピンのタイトル</h2>
+          </div>
+          <div>
+            <div className="commentContainer">
+              <div>
+                <ChatContainer
+                  pinID={this.state.pinID}
+                  messages={this.state.messages}
+                  syncMessage={this.syncMessage}
+                />
+              </div>
+            </div>
+          </div>  
         </div>
       </div>
       </>
