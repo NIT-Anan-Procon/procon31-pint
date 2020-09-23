ca1a3301229d9cbcc666d34b9a9a9819edb10eec
TatsumiKotaro <1186787@st.anan-nct.ac.jp>
2020-09-23 13:15:42 +0900

    修復

--- src/PinController.js
+++ src/PinController.js
@@ -6,14 +6,11 @@ function PinController(props) {
 	// }
 	
 	return (
-		{/* <div className="circle question" onClick={() => props.addPin(props.getVideoTime(), "0")}></div>
+		<>
+		<div className="circle question" onClick={() => props.addPin(props.getVideoTime(), "0")}></div>
 		<div className="circle understanding" onClick={() => props.addPin(props.getVideoTime(), "1")}></div>
-		<div className="circle exclamation" onClick={() => props.addPin(props.getVideoTime(), "2")}></div> */}
-		<div className="pinbutton" onClick={() => props.addPin(props.getVideoTime(),"0")}>
-			<svg width="150" height="150" viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="75" cy="75" r="75" fill="#FE5600"/>
-      </svg>
-		</div>
+		<div className="circle exclamation" onClick={() => props.addPin(props.getVideoTime(), "2")}></div> 
+		</>
 	)
 }
 
--- src/YoutubePlayer.js
+++ src/YoutubePlayer.js
@@ -113,11 +113,7 @@ class YoutubePlayer extends React.Component {
                 </svg>
               </div>
             </div>
-            <PinController
-            getVideoTime={() => Math.round(this.state.videoEl.target.getCurrentTime())}
-            addPin={(time, type) => this.addPin(time, type)}
-          />
-            {/* <div className="pinbutton">
+            <div className="pinbutton">
               <svg width="150" height="150" viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="75" cy="75" r="75" fill="#FE5600"/>
               </svg>
@@ -125,8 +121,8 @@ class YoutubePlayer extends React.Component {
                 <svg width="68" height="100" viewBox="0 0 68 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M33.6633 0C15.0691 0 0 15.0693 0 33.6633C0 52.6402 9.40586 56.6832 19.802 69.3068C32.2367 84.4064 33.6633 100 33.6633 100C33.6633 100 35.0898 84.4064 47.5246 69.307C57.9207 56.6832 67.3266 52.6402 67.3266 33.6635C67.3266 15.0693 52.2574 0 33.6633 0ZM33.6633 45.2971C27.2381 45.2971 22.0297 40.0885 22.0297 33.6635C22.0297 27.2385 27.2383 22.0299 33.6633 22.0299C40.0883 22.0299 45.2969 27.2385 45.2969 33.6635C45.2969 40.0885 40.0883 45.2971 33.6633 45.2971Z" fill="white"/>
                 </svg>
-    </div> 
-    </div>*/}
+              </div> 
+            </div>
           </div>
           <div className="pin">
             {this.state.pins.map((pin, index) => {
