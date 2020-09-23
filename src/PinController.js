65805dd4e1bafd70577eb1b1311e66397efd72aa
TatsumiKotaro <1186787@st.anan-nct.ac.jp>
2020-09-23 13:10:46 +0900

    merge作業前

--- src/PinController.js
+++ src/PinController.js
@@ -4,13 +4,16 @@ function PinController(props) {
 	// let alertMessage = (label) => {
 	//   alert(`${label}\n現在時刻 : ${props.getVideoTime()}秒`);
 	// }
-  
+	
 	return (
-	  <div className="pin" style={{marginTop: "10px"}}>
-		<div className="circle question" onClick={() => props.addPin(props.getVideoTime(), "0")}></div>
+		{/* <div className="circle question" onClick={() => props.addPin(props.getVideoTime(), "0")}></div>
 		<div className="circle understanding" onClick={() => props.addPin(props.getVideoTime(), "1")}></div>
-		<div className="circle exclamation" onClick={() => props.addPin(props.getVideoTime(), "2")}></div>
-	  </div>
+		<div className="circle exclamation" onClick={() => props.addPin(props.getVideoTime(), "2")}></div> */}
+		<div className="pinbutton" onClick={() => props.addPin(props.getVideoTime(),"0")}>
+			<svg width="150" height="150" viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="75" cy="75" r="75" fill="#FE5600"/>
+      </svg>
+		</div>
 	)
 }
 
