8bc18842b0165a4ad329f16301945058b0a734cf
Naoki Takahashi <43571743+naoki-tkhs@users.noreply.github.com>
2021-01-14 01:03:52 +0900

    Update script.js

--- API/script.js
+++ API/script.js
@@ -1,5 +1,5 @@
 $("#search").on('click', function () {
-	const APIKEY = "1a31f7c4a011fa3551cf9a39d64f4a23";
+	const APIKEY = "";
 	var city = $("#city").val();
 	console.log(city);
 	var url = 'http://api.openweathermap.org/data/2.5/weather';
